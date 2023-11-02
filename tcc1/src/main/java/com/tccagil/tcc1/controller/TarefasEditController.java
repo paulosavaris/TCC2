12ff1dc4698fdfe3dde221138a5742fd6f875abe
paulosavaris <pauloosavaris@gmail.com>
2023-11-02 15:14:58 -0300

    Busca dos dados da tarefas para editar

--- tcc1/src/main/java/com/tccagil/tcc1/controller/TarefasEditController.java
+++ tcc1/src/main/java/com/tccagil/tcc1/controller/TarefasEditController.java
@@ -6,24 +6,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 import com.tccagil.tcc1.Service.AutenticacaoService;
-
-
+import com.tccagil.tcc1.domain.tarefas.TarefasDao;
+import com.tccagil.tcc1.domain.tarefas.TarefasRepository;
 
 import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class TarefasEditController {
 
     @Autowired
     private AutenticacaoService autenticacaoService;
 
+    @Autowired
+    private TarefasRepository tarefasRepository;
+
     @GetMapping("/tarefasEdit/{idtarefas}")
-    public String altTarefa(@PathVariable Long idtarefas , HttpSession session, Model model) {
+    public String exibirTarefa(@PathVariable Long idtarefas, HttpSession session, Model model) {
         if (autenticacaoService.isUsuarioLogado(session)) {
             autenticacaoService.adicionarInformacoesComuns(model, session);
-            
-            int idUsuario = (int) session.getAttribute("idUsuarioLogado");
+
+            TarefasDao tarefa = tarefasRepository.findById(idtarefas).orElse(null);
+            model.addAttribute("tarefa", tarefa);
 
             model.addAttribute("idtarefas", idtarefas);
             return "tarefasEdit";
@@ -31,6 +35,5 @@ public class TarefasEditController {
             return "redirect:/login";
         }
     }
-
 
 }
