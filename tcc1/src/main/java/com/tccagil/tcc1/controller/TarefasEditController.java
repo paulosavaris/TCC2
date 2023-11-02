971046a06affae4c45f15c3f0f72e73f73c40320
paulosavaris <pauloosavaris@gmail.com>
2023-11-02 20:39:34 -0300

    sei la

--- tcc1/src/main/java/com/tccagil/tcc1/controller/TarefasEditController.java
+++ tcc1/src/main/java/com/tccagil/tcc1/controller/TarefasEditController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.tccagil.tcc1.Service.AutenticacaoService;
 import com.tccagil.tcc1.domain.tarefas.AtivadeRepository;
@@ -46,37 +47,58 @@ public class TarefasEditController {
     }
 
     @PostMapping("/tarefasEdit/{idtarefas}")
-    public String atualizarTarefa(@PathVariable Long idtarefas, AtividadeDTO dados, HttpSession session) {
+    public String atualizarTarefa(@PathVariable Long idtarefas, AtividadeDTO dados, TarefasDTO form,
+            HttpSession session, RedirectAttributes redirectAttributes) {
 
+        String descricaoAtividade = dados.getDescricaoAtividade();
         int usuarioIdAtividade = (int) session.getAttribute("idUsuarioLogado");
+        TarefasDao tarefa = tarefasRepository.findById(idtarefas).orElse(null);
 
-        AtividadesRecord novaAtividade = new AtividadesRecord(dados.getDescricaoAtividade(), idtarefas, usuarioIdAtividade);
+        if (descricaoAtividade != null && !descricaoAtividade.isEmpty()){
+        AtividadesRecord novaAtividade = new AtividadesRecord(dados.getDescricaoAtividade(), idtarefas,
+                usuarioIdAtividade);
 
         AtividadeDao atividade = new AtividadeDao(novaAtividade);
         ativadeRepository.save(atividade);
-        return "redirect:/tarefasEdit/{idtarefas}";
-    }
+                // Adicione uma mensagem de sucesso
+                redirectAttributes.addFlashAttribute("sucesso", "Atividade cadastrada com sucesso!");
+        }
 
+        if (tarefa != null && !tarefa.isEqual(form)) { // Implemente o método isEqual() na classe TarefasDao
+            // // Atualize a entidade apenas se os campos forem diferentes
+            tarefa.setTitulo(form.getTarefaNomeEdit());
+            tarefa.setDescricao(form.getTarefaDescricaoEdit());
+            tarefa.setPrioridade(form.getTarefaPrioridadeEdit());
+            tarefa.setStatus(form.getStatusTarefasEdit());
 
+            // Atualize a tarefa no banco de dados
+            tarefasRepository.save(tarefa);
+                            // Adicione uma mensagem de sucesso
+                redirectAttributes.addFlashAttribute("sucesso", "Atividade cadastrada com sucesso!");
+            }
+            return "redirect:/tarefasEdit/{idtarefas}";
+        }
 
     // @PostMapping("/tarefasEdit/{idtarefas}")
-    // public String atualizarTarefa(@PathVariable Long idtarefas, TarefasDTO form) {
-    //     TarefasDao tarefa = tarefasRepository.findById(idtarefas).orElse(null);
+    // public String atualizarTarefa(@PathVariable Long idtarefas, TarefasDTO form)
+    // {
+    // TarefasDao tarefa = tarefasRepository.findById(idtarefas).orElse(null);
 
-    //     // Verifique se os campos foram alterados em relação aos valores atuais
-    //     if (tarefa != null && !tarefa.isEqual(form)) { // Implemente o método isEqual() na classe TarefasDao
-    //         // Atualize a entidade apenas se os campos forem diferentes
-    //         tarefa.setTitulo(form.getTarefaNomeEdit());
-    //         tarefa.setDescricao(form.getTarefaDescricaoEdit());
-    //         tarefa.setPrioridade(form.getTarefaPrioridadeEdit());
-    //         tarefa.setStatus(form.getStatusTarefasEdit());
+    // // Verifique se os campos foram alterados em relação aos valores atuais
+    // if (tarefa != null && !tarefa.isEqual(form)) { // Implemente o método
+    // isEqual() na classe TarefasDao
+    // // Atualize a entidade apenas se os campos forem diferentes
+    // tarefa.setTitulo(form.getTarefaNomeEdit());
+    // tarefa.setDescricao(form.getTarefaDescricaoEdit());
+    // tarefa.setPrioridade(form.getTarefaPrioridadeEdit());
+    // tarefa.setStatus(form.getStatusTarefasEdit());
 
-    //         // Atualize a tarefa no banco de dados
-    //         tarefasRepository.save(tarefa);
-    //     }
+    // // Atualize a tarefa no banco de dados
+    // tarefasRepository.save(tarefa);
+    // }
 
-    //     // Redirecione de volta para a página de edição da tarefa
-    //     return "redirect:/tarefasEdit/{idtarefas}";
+    // // Redirecione de volta para a página de edição da tarefa
+    // return "redirect:/tarefasEdit/{idtarefas}";
     // }
 
 }
